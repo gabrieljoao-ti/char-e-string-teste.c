e1f6a95d4177efb8b7c72905d8e79f0da0bc0b9c
João Gabriel Rocha da Conceição <eugabrielrochaconceicao@gmail.com>
2026-05-21 10:08:08 -0300

    Add password validation with length and character checks

---
+++ senha_com_letramaiuscula.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+
+int main() { 
+
+    char senha[40];
+    int tem_numero = 0;
+    int letra_maiuscula = 0; 
+    int tamanho = 0;
+
+    printf("Cadastre sua nova senha: "); 
+    scanf("%s", senha);
+
+    tamanho = strlen(senha);
+
+    for(int i = 0; i < tamanho; i++){
+        if(senha[i] >= '0' && senha[i] <= '9'){
+            tem_numero = 1;
+        }
+        if(senha[i] >= 'A' && senha[i] <= 'Z'){
+             letra_maiuscula = 1; 
+        }
+    }
+
+    printf("\n---Analise de senha:---\n");
+
+    
+    if(tamanho >= 6 && tem_numero == 1 && letra_maiuscula == 1){
+        printf("Senha valida e super segura!\n");
+    } else {
+        printf("Senha Invalida!\n");
+        printf("Motivo da Invalidade:");
+
+        if(tamanho < 6) {
+            printf("\n--A senha deve ter no minimo 6 caracteres--\n");
+        }
+        if(tem_numero == 0) {
+            printf("\n--A senha deve ter pelo menos um numero--\n"); 
+        }
+        
+        if(letra_maiuscula == 0) {
+            printf("\n--A senha deve ter pelo menos uma letra maiuscula--\n"); 
+        }
+    }
+
+    system("pause");
+    return 0;
+}
